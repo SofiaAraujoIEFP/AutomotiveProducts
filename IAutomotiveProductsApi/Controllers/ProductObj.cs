cb7bbe371a6e23b8d53e0c72a30dfd6f6a131acf
35191 <sofia_araujo_gdl@hotmail.com>
2025-02-17 18:16:17 +0000

    metodo save incompleto

---
+++ IAutomotiveProductsApi/Controllers/ProductObj.cs
@@ -0,0 +1,14 @@
+﻿namespace IAutomotiveProductsApi.Controllers
+{
+    internal class ProductObj
+    {
+        public ProductObj()
+        {
+        }
+
+        public object Descrition { get; internal set; }
+        public object Title { get; internal set; }
+        public DateTime CreatedAt { get; internal set; }
+        public DateTime UpdateAt { get; internal set; }
+    }
+}
